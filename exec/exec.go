8cbd2a17eee70c0118059aaa6bf7a7d518117fb4
admin <admin@mail.com>
2024-04-17 00:12:14 +0800

    2024-04-12

--- exec/exec.go
+++ exec/exec.go
@@ -1,49 +1,40 @@
 package exec
 
+import (
+	"context"
+	"io"
+	"os/exec"
+)
 
-func (e *Error) Error() string { //md5:47e007c6da65537c1ce793159301e0d2
+//func (e *Error) Error() string { //md5:47e007c6da65537c1ce793159301e0d2
+//	return e.F.Error()
+//}
+//
+//func (e *Error) Unwrap() error { //md5:fe0424a855c24b4387d85783d12d42d2
+//	return e.F.Unwrap()
+//}
 
+type Cmd struct {
+	F exec.Cmd
 }
 
-
-func (e *Error) Unwrap() error { //md5:fe0424a855c24b4387d85783d12d42d2
-
-}
-
-
-func (w wrappedError) Error() string { //md5:299c35975f0c83bd3edb8bb7999594a2
-
-}
-
-
-func (w wrappedError) Unwrap() error { //md5:688743fbe0cc4b9c4cd1c9455395791b
-
-}
-
-// Command returns the Cmd struct to execute the named program with
-// the given arguments.
+// Command 返回Cmd结构以使用给定参数执行命名程序。
 //
-// It sets only the Path and Args in the returned structure.
+// 它只设置返回结构中的Path和Args。
 //
-// If name contains no path separators, Command uses LookPath to
-// resolve name to a complete path if possible. Otherwise it uses name
-// directly as Path.
+// 若名称不包含路径分隔符，则命令使用LookPath将名称解析为完整的路径（如果可能）。否则，它直接使用名称作为路径。
 //
-// The returned Cmd's Args field is constructed from the command name
-// followed by the elements of arg, so arg should not include the
-// command name itself. For example, Command("echo", "hello").
-// Args[0] is always name, not the possibly resolved Path.
+// 返回的Cmd的Args字段由命令名后跟arg元素构成， 因此arg不应包含命令名本身. 例如, I设置命令("echo", "hello").
+// Args[0] 始终是名称，而不是可能解析的路径。
 //
-// On Windows, processes receive the whole command line as a single string
-// and do their own parsing. Command combines and quotes Args into a command
-// line string with an algorithm compatible with applications using
-// CommandLineToArgvW (which is the most common way). Notable exceptions are
-// msiexec.exe and cmd.exe (and thus, all batch files), which have a different
-// unquoting algorithm. In these or other similar cases, you can do the
-// quoting yourself and provide the full command line in SysProcAttr.CmdLine,
-// leaving Args empty.
+// 在Windows上，进程以单个字符串的形式接收整个命令行，并执行自己的解析.
+// 命令使用与使用CommandLineToArgvW的应用程序兼容的算法（这是最常见的方法）将Args组合并引用到命令行字符串中.
+// 值得注意的例外是msiexec.exe和cmd.exe（以及所有批处理文件）， 它们具有不同的去激励算法。
+// 在这些或其他类似情况下, 您可以自己引用，并在SysProcAttr.CmdLine中提供完整的命令行，将Args留空。
 func Command(name string, arg ...string) *Cmd { //md5:2912017d0548a6188a67825cb0633059
-
+	return &Cmd{
+		F: *exec.Command(name, arg...),
+	}
 }
 
 // CommandContext 类似于 Command，但包含了上下文信息。
@@ -54,7 +45,9 @@ func Command(name string, arg ...string) *Cmd { //md5:2912017d0548a6188a67825cb0
 // CommandContext 将命令的 Cancel 函数设置为调用其 Process 的 Kill 方法，
 // 并将其 WaitDelay 保持未设置状态。在启动命令之前，调用者可以通过修改这些字段来改变取消行为。
 func CommandContext(ctx context.Context, name string, arg ...string) *Cmd { //md5:6482c2ac4f2da9383d162ac6194f5a52
-
+	return &Cmd{
+		F: *exec.CommandContext(ctx, name, arg...),
+	}
 }
 
 // String 返回对 c 的人类可读的描述。
@@ -62,7 +55,7 @@ func CommandContext(ctx context.Context, name string, arg ...string) *Cmd { //md
 // 特别地，它不适合用作 shell 的输入。
 // String 的输出可能会在不同的 Go 版本之间有所变化。
 func (c *Cmd) String() string { //md5:81f6a84e6ffe77c0a4beb3a6ebcb002f
-
+	return c.F.String()
 }
 
 // Run 方法启动指定的命令并等待其完成。
@@ -73,7 +66,7 @@ func (c *Cmd) String() string { //md5:81f6a84e6ffe77c0a4beb3a6ebcb002f
 //
 // 如果调用goroutine通过runtime.LockOSThread锁定操作系统线程并对任何可继承的操作系统级线程状态进行了修改（例如Linux或Plan 9命名空间），则新进程将继承调用者的线程状态。
 func (c *Cmd) Run() error { //md5:3c28fe2808b00288feef65a1a9bcc0df
-
+	return c.F.Run()
 }
 
 // Start 方法启动指定的命令，但不会等待其完成执行。
@@ -82,12 +75,15 @@ func (c *Cmd) Run() error { //md5:3c28fe2808b00288feef65a1a9bcc0df
 //
 // 在成功调用 Start 方法后，必须调用 Wait 方法来释放相关的系统资源。
 func (c *Cmd) Start() error { //md5:e4e960bd99b0815c5f1d6601ec662dc4
-
+	return c.F.Start()
 }
 
+type ExitError struct {
+	F exec.ExitError
+}
 
 func (e *ExitError) Error() string { //md5:e7d8165bbebf499178bc1e1a3a045796
-
+	return e.F.Error()
 }
 
 // Wait 等待命令退出，并等待向标准输入复制或从标准输出、标准错误复制的任何操作完成。
@@ -102,19 +98,19 @@ func (e *ExitError) Error() string { //md5:e7d8165bbebf499178bc1e1a3a045796
 //
 // Wait 释放与 Cmd 关联的所有资源。
 func (c *Cmd) Wait() error { //md5:c1cc5cd3a68d4db76704a0a00058acc4
-
+	return c.F.Wait()
 }
 
 // Output 运行命令并返回其标准输出。
 // 返回的任何错误通常会是 *ExitError 类型。
 // 若 c.Stderr 为 nil，Output 会填充 ExitError.Stderr。
 func (c *Cmd) Output() ([]byte, error) { //md5:6c0c99752f0c5a95d947693df1a82db1
-
+	return c.F.Output()
 }
 
 // CombinedOutput 执行命令并返回其标准输出和标准错误的合并结果。
 func (c *Cmd) CombinedOutput() ([]byte, error) { //md5:fa60c475e01331ed5a06057e7774b761
-
+	return c.F.CombinedOutput()
 }
 
 // StdinPipe 返回一个管道，该管道将在命令启动时连接到命令的标准输入。
@@ -122,34 +118,24 @@ func (c *Cmd) CombinedOutput() ([]byte, error) { //md5:fa60c475e01331ed5a06057e7
 // 调用者只需调用Close即可强制管道提前关闭。
 // 例如，如果运行的命令在标准输入关闭之前不会退出，则调用者必须关闭该管道。
 func (c *Cmd) StdinPipe() (io.WriteCloser, error) { //md5:a46d29cb606f6af4122775095f43f1d6
-
+	return c.F.StdinPipe()
 }
 
 // StdoutPipe 返回一个管道，当命令启动时，该管道将与命令的标准输出相连。
 //
 // 当Wait观察到命令退出后，会关闭该管道，因此大多数调用者无需自行关闭管道。因此，在完成从管道的所有读取操作之前调用Wait是不正确的。出于同样的原因，在使用StdoutPipe时调用Run也是不正确的。有关惯用用法，请参阅示例。
 func (c *Cmd) StdoutPipe() (io.ReadCloser, error) { //md5:ddeb49a7d48e93da480da5299d2eed2b
-
+	return c.F.StdoutPipe()
 }
 
 // StderrPipe 返回一个管道，当命令启动时，该管道将与命令的标准错误相连。
 //
 // 当Wait检测到命令退出后，会关闭该管道，因此大多数调用者无需自行关闭管道。因此，在完成从管道的所有读取操作之前调用Wait是不正确的。出于同样的原因，当使用StderrPipe时，调用Run也是不正确的。有关惯用用法，请参阅StdoutPipe示例。
 func (c *Cmd) StderrPipe() (io.ReadCloser, error) { //md5:9b10883945ef55930f9026118bde260b
-
-}
-
-
-func (w *prefixSuffixSaver) Write(p []byte) (n int, err error) { //md5:b96f3a5712d84fa3fbc26437e496280b
-
-}
-
-
-func (w *prefixSuffixSaver) Bytes() []byte { //md5:66c73a4889d14bec0dceec6067acac38
-
+	return c.F.StderrPipe()
 }
 
 // Environ 返回当前配置下，命令将要运行的环境副本。
 func (c *Cmd) Environ() []string { //md5:891b61dc22bc81880c267878539c842b
-
+	return c.F.Environ()
 }
--- exec/lp.go
+++ exec/lp.go
@@ -1,5 +1,9 @@
 package exec
 
+import (
+	"os/exec"
+)
+
 // LookPath 在由 path 环境变量指定的目录中搜索名为 file 的可执行文件。
 // 如果 file 以 "/"、"#"、"./" 或 "../" 开头，会直接尝试查找，而不参考路径。
 // 成功时，结果为一个绝对路径。
@@ -7,5 +11,5 @@ package exec
 // 在 Go 的早期版本中，LookPath 可能会返回相对于当前目录的路径。
 // 自 Go 1.19 起，LookPath 将改为返回该路径以及一个满足 errors.Is(err, ErrDot) 的错误。更多详情请参阅包文档。
 func LookPath(file string) (string, error) { //md5:154a1e41bca1510f44308fb5c079e855
-
+	return exec.LookPath(file)
 }
